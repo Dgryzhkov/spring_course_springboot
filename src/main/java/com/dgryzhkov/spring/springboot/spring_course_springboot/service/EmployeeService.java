2a9f8609153bf6df806107c2daaae5297909b4d1
Denis Ryzhkov <dgryzhkov@gmail.com>
2022-09-17 13:45:19 +0700

    rest P1

---
+++ src/main/java/com/dgryzhkov/spring/springboot/spring_course_springboot/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.dgryzhkov.spring.springboot.spring_course_springboot.service;
+
+
+import com.dgryzhkov.spring.springboot.spring_course_springboot.entity.Employee;
+
+import java.util.List;
+
+/**
+ * @author Dgryzhkov
+ */
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+//
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//    public void deleteEmployee(int id);
+}
